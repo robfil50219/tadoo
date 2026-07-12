3fa0f5a3634bf2874f7a18daa3da3780bcacd2c1
robfil50219 <132905349+robfil50219@users.noreply.github.com>
2026-07-12 20:37:02 +0200

    Add allowedDevOrigins for local development and create ESLint configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ['127.0.0.1', '10.0.0.12'],
 };
 
 module.exports = nextConfig;
